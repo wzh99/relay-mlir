96615e9cd2e7633a4883b2d53f9999a9fd654964
wzh99 <wzh1999_frog@126.com>
2022-09-27 10:14:23 +0800

    Finish operator fusion.

--- bin/test.cpp
+++ bin/test.cpp
@@ -11,11 +11,14 @@
 #include "tvm/relay/expr.h"
 
 auto code = R"d(#[version = "0.0.5"]
-def @main(%x: Tensor[(1, 2), float32], %w: Tensor[(2, 2), float32], %b: Tensor[(2), float32]) {
-    %0 = nn.dense(%x, %w, units=None);
-    %1 = nn.bias_add(%0, %b, axis=1);
+def @main(%x: Tensor[(1, 2), float32], %w1: Tensor[(2, 2), float32], %b1: Tensor[(2), float32],
+    %w2: Tensor[(4, 2), float32], %b2: Tensor[(4), float32]) {
+    %0 = nn.dense(%x, %w1, units=None);
+    %1 = nn.bias_add(%0, %b1, axis=1);
     %2 = nn.relu(%1);
-    %2
+    %3 = nn.dense(%2, %w2, units=None);
+    %4 = nn.bias_add(%3, %b2, axis=1);
+    %4
 })d";
 
 using namespace mlir;
@@ -28,6 +31,6 @@ int main(int argc, char const *argv[]) {
     PassManager pm(&ctx, PassManager::Nesting::Implicit);
     pm.addPass(relay::createShapeInference());
     pm.addPass(relay::createOpFusion());
-    if (pm.run(mod).failed()) Fatal("Failed to run passes on module.");
+    pm.run(mod).succeeded();
     mod.dump();
 }
--- lib/Dialect/Relay/Transforms/OpFusion.cpp
+++ lib/Dialect/Relay/Transforms/OpFusion.cpp
@@ -1,6 +1,7 @@
 #include <unordered_map>
 
 #include "PassDetail.hpp"
+#include "mlir/IR/BlockAndValueMapping.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
@@ -11,8 +12,7 @@ namespace mlir {
 namespace relay {
 
 static LogicalResult fuseDenseBiasAddRelu(Operation *pivot,
-                                          SmallVector<Operation *> &group,
-                                          SmallVector<Operation *> &outputs) {
+                                          SmallVector<Operation *> &group) {
     // Match dense
     if (pivot->getName().getStringRef() != "relay.nn.dense") return failure();
 
@@ -23,7 +23,6 @@ static LogicalResult fuseDenseBiasAddRelu(Operation *pivot,
     if (biasAdd->getName().getStringRef() != "relay.nn.bias_add")
         return failure();
     group = {pivot, biasAdd};
-    outputs = {biasAdd};
 
     // Optionally match relu
     auto biasAddResult = biasAdd->getResult(0);
@@ -31,13 +30,11 @@ static LogicalResult fuseDenseBiasAddRelu(Operation *pivot,
     auto relu = llvm::to_vector(biasAddResult.getUsers())[0];
     if (relu->getName().getStringRef() != "relay.nn.relu") return success();
     group.push_back(relu);
-    outputs = {relu};
 
     return success();
 }
 
-using MatchFn = LogicalResult (*)(Operation *, SmallVector<Operation *> &,
-                                  SmallVector<Operation *> &);
+using MatchFn = LogicalResult (*)(Operation *, SmallVector<Operation *> &);
 static MatchFn matchFuncs[] = {fuseDenseBiasAddRelu};
 
 struct FusionGroup {
@@ -53,17 +50,97 @@ public:
           groups(groups),
           opGrpIdx(opGrpIdx) {}
 
-    LogicalResult matchAndRewrite(Operation *op,
-                                  PatternRewriter &rewriter) const override {
-        // Find out the group
-        Debug("{}", op->getName().getStringRef());
-        return success();
-    }
+    LogicalResult matchAndRewrite(Operation *root,
+                                  PatternRewriter &rewriter) const override;
 
 private:
+    mutable size_t nGroup = 0;
     const std::vector<FusionGroup> &groups;
     const std::unordered_map<Operation *, size_t> &opGrpIdx;
 };
+
+LogicalResult OpFusionPattern::matchAndRewrite(
+    Operation *root, PatternRewriter &rewriter) const {
+    // Find out the group
+    if (root->getDialect()->getNamespace() != "relay") return success();
+    if (cast<func::FuncOp>(root->getParentOp()).getName() != "main")
+        return success();
+    auto &group = groups[opGrpIdx.at(root)];
+
+    // Only rewrite at outputs
+    if (!llvm::is_contained(group.outputs, root)) return success();
+
+    // Find all arguments of the new function
+    DenseSet<Operation *> opSet(group.ops.begin(), group.ops.end());
+    std::vector<Value> args;
+    for (auto op : group.ops)
+        for (auto arg : op->getOperands())
+            if (!opSet.contains(arg.getDefiningOp())) args.push_back(arg);
+
+    // Find all results of the new function
+    std::vector<Value> results;
+    std::vector<std::pair<size_t, size_t>> resultIndices;
+    for (auto outOpZip : llvm::enumerate(group.outputs)) {
+        for (auto resultZip : llvm::enumerate(outOpZip.value()->getResults())) {
+            auto result = resultZip.value();
+            if (llvm::any_of(result.getUsers(), [&](Operation *op) {
+                    return !opSet.contains(op);
+                })) {
+                results.push_back(result);
+                resultIndices.push_back({outOpZip.index(), resultZip.index()});
+            }
+        }
+    }
+
+    // Create prototype of the function
+    auto inTypes = llvm::to_vector(
+        llvm::map_range(args, [](Value in) { return in.getType(); }));
+    auto outTypes = llvm::to_vector(
+        llvm::map_range(results, [](Value out) { return out.getType(); }));
+    auto funcType = rewriter.getFunctionType(inTypes, outTypes);
+    auto funcName = fmt::format("fused_{}", nGroup++);
+    rewriter.setInsertionPointToEnd(root->getParentOp()->getBlock());
+    auto func =
+        rewriter.create<func::FuncOp>(root->getLoc(), funcName, funcType);
+    func->setAttr("primitive", rewriter.getBoolAttr(true));
+
+    // Create function body
+    auto block = func.addEntryBlock();
+    BlockAndValueMapping mapper;
+    for (auto [arg, param] : llvm::zip(args, block->getArguments()))
+        mapper.map(arg, param);
+    rewriter.setInsertionPointToStart(block);
+    std::vector<Operation *> funcOutputs;
+    for (auto op : group.ops) {
+        auto clonedOp = rewriter.clone(*op, mapper);
+        if (llvm::is_contained(group.outputs, op))
+            funcOutputs.push_back(clonedOp);
+    }
+    SmallVector<Value> funcResults;
+    for (auto [i, j] : resultIndices)
+        funcResults.push_back(funcOutputs[i]->getResult(j));
+    rewriter.create<func::ReturnOp>(root->getLoc(), funcResults);
+
+    // Replace group with function call
+    rewriter.setInsertionPointAfter(root);
+    auto funcCall = rewriter.create<func::CallOp>(
+        root->getLoc(), FlatSymbolRefAttr::get(func), outTypes, args);
+
+    // Replace uses of group outputs
+    auto callResults = funcCall.getResults();
+    auto indexIter = resultIndices.begin();
+    for (auto opZip : llvm::enumerate(group.outputs)) {
+        auto begin = indexIter;
+        while (indexIter->first == opZip.index()) ++indexIter;
+        if (begin == indexIter) continue;
+        SmallVector<Value> newValues;
+        for (auto [_, j] : llvm::iterator_range(begin, indexIter))
+            newValues.push_back(callResults[j]);
+        rewriter.replaceOp(opZip.value(), newValues);
+    }
+
+    return success();
+}
 
 class OpFusion : public OpFusionBase<OpFusion> {
     void runOnOperation() override;
@@ -77,40 +154,47 @@ void OpFusion::runOnOperation() {
 
     // Match and fuse patterns
     SmallVector<Operation *> groupOps;
-    SmallVector<Operation *> outputs;
     std::vector<FusionGroup> groups;
     std::unordered_map<Operation *, size_t> opGrpIdx;
 
-    mainFn.walk([&](Operation *op) {
+    mainFn.walk([&](Operation *pivot) {
         // Skip operations not interested
-        if (op->getDialect()->getNamespace() != "relay") return;
-        if (opGrpIdx.count(op)) return;
+        if (pivot->getDialect()->getNamespace() != "relay") return;
+        if (opGrpIdx.count(pivot)) return;
 
         for (auto matcher : matchFuncs) {
             // Match with predefined functions
-            if (matcher(op, groupOps, outputs).failed()) continue;
-            assert(!groupOps.empty() && !outputs.empty());
+            if (matcher(pivot, groupOps).failed()) continue;
+            assert(!groupOps.empty());
 
             // Create fusion group
             FusionGroup group;
             group.ops.swap(groupOps);
-            group.outputs.swap(outputs);
+            DenseSet<Operation *> opSet(group.ops.begin(), group.ops.end());
+            for (auto op : group.ops) {
+                auto isOut = false;
+                for (auto result : op->getResults())
+                    for (auto user : result.getUsers())
+                        if (!opSet.contains(user)) isOut = true;
+                if (isOut) group.outputs.push_back(op);
+            }
             auto grpIdx = groups.size();
-            for (auto gOp : group.ops) opGrpIdx.insert({gOp, grpIdx});
+            for (auto op : group.ops) opGrpIdx.insert({op, grpIdx});
             groups.push_back(std::move(group));
             return;
         }
 
         // Create single-operation group
-        opGrpIdx.insert({op, groups.size()});
-        groups.push_back({.ops = {op}, .outputs = {op}});
+        opGrpIdx.insert({pivot, groups.size()});
+        groups.push_back({.ops = {pivot}, .outputs = {pivot}});
     });
 
     // Create nested function for each group
     RewritePatternSet patterns(ctx);
     patterns.add<OpFusionPattern>(ctx, groups, opGrpIdx);
     FrozenRewritePatternSet frozenPat(std::move(patterns));
-    applyPatternsAndFoldGreedily(mainFn, frozenPat, {.maxIterations = 0})
+    GreedyRewriteConfig config{.useTopDownTraversal = true, .maxIterations = 0};
+    applyPatternsAndFoldGreedily(mainFn, frozenPat, std::move(config))
         .succeeded();
 }
 
