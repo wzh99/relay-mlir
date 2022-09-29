a6a0c17b3f79aaab028411a741780b52035189c6
wzh99 <wzh1999_frog@126.com>
2022-09-29 16:39:06 +0800

    Lower `nn.dense`.

--- lib/Conversion/RelayToAffine/RelayToAffine.cpp
+++ lib/Conversion/RelayToAffine/RelayToAffine.cpp
@@ -13,6 +13,70 @@ inline static Type cvtTensorToMemref(Type type) {
     return MemRefType::get(tt.getShape(), tt.getElementType());
 }
 
+template <class Op>
+struct LowerOp : public OpRewritePattern<Op> {
+    LowerOp(MLIRContext *ctx) : OpRewritePattern<Op>(ctx) {}
+
+    LogicalResult matchAndRewrite(Op op,
+                                  PatternRewriter &rewriter) const override;
+
+    virtual LogicalResult lower(Op op, ValueRange buffers,
+                                PatternRewriter &rewriter) const = 0;
+};
+
+template <class Op>
+LogicalResult LowerOp<Op>::matchAndRewrite(Op op,
+                                           PatternRewriter &rewriter) const {
+    // Find buffers for function outputs
+    SmallVector<Value> newResults;
+    bool isFuncRet = false;
+
+    for (auto result : op->getResults()) {
+        // Check if the result of this operation is returned by the parent
+        // function
+        Value newResult;
+        func::ReturnOp retOp;
+        int64_t retIdx = -1;
+        for (auto &use : result.getUses()) {
+            auto owner = use.getOwner();
+            if (owner->getName().getStringRef() ==
+                func::ReturnOp::getOperationName()) {
+                retOp = cast<func::ReturnOp>(owner);
+                retIdx = use.getOperandNumber();
+                isFuncRet = true;
+            }
+        }
+
+        // Collect result buffer or allocate a new one
+        if (retOp) {
+            auto func = cast<func::FuncOp>(op->getParentOp());
+            auto numInputs =
+                func->template getAttrOfType<IntegerAttr>("num_inputs")
+                    .getInt();
+            newResult = func.getArgument(numInputs + retIdx);
+        } else {
+            auto alloca = rewriter.create<memref::AllocaOp>(
+                op.getLoc(), result.getType().template cast<MemRefType>());
+            newResult = alloca.getResult();
+        }
+        newResults.push_back(newResult);
+    }
+
+    auto buffers = llvm::to_vector(op.getOperands());
+    buffers.append(newResults);
+
+    // Lower operation with given buffers
+    if (this->lower(op, buffers, rewriter).failed()) return failure();
+
+    // Erase or replace previous operations
+    if (isFuncRet)
+        op.erase();
+    else
+        rewriter.replaceOp(op, newResults);
+
+    return success();
+}
+
 struct LowerReLU : public OpRewritePattern<relay::ReLUOp> {
     LowerReLU(MLIRContext *ctx) : OpRewritePattern(ctx) {}
 
@@ -31,66 +95,71 @@ struct LowerBiasAdd : public OpRewritePattern<relay::BiasAddOp> {
     }
 };
 
-struct LowerDense : public OpRewritePattern<relay::DenseOp> {
-    LowerDense(MLIRContext *ctx) : OpRewritePattern(ctx) {}
+#define FOR(name, low, high, body)                                         \
+    auto name##For = rewriter.create<AffineForOp>(op.getLoc(), low, high); \
+    rewriter.setInsertionPointToStart(name##For.getBody());                \
+    {                                                                      \
+        auto name##Iv = name##For.getInductionVar();                       \
+        body                                                               \
+    }                                                                      \
+    rewriter.setInsertionPoint(name##For.getBody()->getTerminator());
 
-    LogicalResult matchAndRewrite(relay::DenseOp op,
-                                  PatternRewriter &rewriter) const override {
+#define LOAD(buffer, indices) \
+    rewriter.create<AffineLoadOp>(op.getLoc(), buffer, indices).getResult()
+
+#define STORE(value, buffer, indices) \
+    rewriter.create<AffineStoreOp>(op.getLoc(), value, buffer, indices)
+
+#define F32_CONST(value)                                                   \
+    rewriter                                                               \
+        .create<arith::ConstantFloatOp>(op.getLoc(), llvm::APFloat(value), \
+                                        rewriter.getF32Type())             \
+        .getResult();
+
+#define BOP(Op, lhs, rhs) rewriter.create<Op>(op.getLoc(), lhs, rhs).getResult()
+
+#define ADDF(lhs, rhs) BOP(arith::AddFOp, lhs, rhs)
+#define MULF(lhs, rhs) BOP(arith::MulFOp, lhs, rhs)
+
+struct LowerDense : public LowerOp<relay::DenseOp> {
+    LowerDense(MLIRContext *ctx) : LowerOp(ctx) {}
+
+    LogicalResult lower(relay::DenseOp op, ValueRange buffers,
+                        PatternRewriter &rewriter) const override {
+        auto data = buffers[0], weight = buffers[1], result = buffers[2];
+        auto dataShape = data.getType().cast<MemRefType>().getShape();
+        auto weightShape = weight.getType().cast<MemRefType>().getShape();
+        auto batchSize = dataShape[0], inDim = dataShape[1],
+             outDim = weightShape[0];
+
+        FOR(batch, 0, batchSize,  // for (i, 0, data.shape[0])
+            FOR(out, 0, outDim,   // for (j, 0, weight.shape[0])
+                auto init = F32_CONST(1.f);
+                STORE(init, result, (ValueRange{batchIv, outIv}));
+                FOR(in, 0, inDim,  // for (k, 0, data.shape[i])
+                    auto D_ik =
+                        LOAD(data, (ValueRange{batchIv, inIv}));  // data[i, k]
+                    auto W_jk = LOAD(
+                        weight, (ValueRange{outIv, inIv}));  // weight[j, k]
+                    auto mul = MULF(D_ik, W_jk);
+                    auto prev = LOAD(result, (ValueRange{batchIv, outIv}));
+                    auto add = ADDF(prev, mul);
+                    // result[i, j] += data[i, k] * weight[j, k]
+                    STORE(add, result, (ValueRange{batchIv, outIv}));)  // end k
+                )                                                       // end j
+            )                                                           // end i
+
         return success();
     }
 };
 
-struct LowerCall : public OpRewritePattern<func::CallOp> {
-    LowerCall(MLIRContext *ctx) : OpRewritePattern(ctx) {}
+struct LowerCall : public LowerOp<func::CallOp> {
+    LowerCall(MLIRContext *ctx) : LowerOp(ctx) {}
 
-    LogicalResult matchAndRewrite(func::CallOp op,
-                                  PatternRewriter &rewriter) const override {
-        // Find buffers for function outputs
-        SmallVector<Value> newResults;
-        bool isFuncRet = false;
-        for (auto result : op->getResults()) {
-            // Check if the result of this operation is returned by the parent
-            // function
-            Value newResult;
-            func::ReturnOp retOp;
-            int64_t retIdx = -1;
-            for (auto &use : result.getUses()) {
-                auto owner = use.getOwner();
-                if (owner->getName().getStringRef() ==
-                    func::ReturnOp::getOperationName()) {
-                    retOp = cast<func::ReturnOp>(owner);
-                    retIdx = use.getOperandNumber();
-                    isFuncRet = true;
-                }
-            }
-
-            // Collect result buffer or allocate a new one
-            if (retOp) {
-                auto func = cast<func::FuncOp>(op->getParentOp());
-                auto numInputs =
-                    func->getAttrOfType<IntegerAttr>("num_inputs").getInt();
-                newResult = func.getArgument(numInputs + retIdx);
-            } else {
-                auto alloca = rewriter.create<memref::AllocaOp>(
-                    op.getLoc(), result.getType().cast<MemRefType>());
-                newResult = alloca.getResult();
-            }
-            newResults.push_back(newResult);
-        }
-
-        auto buffers = llvm::to_vector(op.getOperands());
-        buffers.append(newResults);
-
-        // Lower call operation
+    LogicalResult lower(func::CallOp op, ValueRange buffers,
+                        PatternRewriter &rewriter) const override {
         rewriter.create<func::CallOp>(op.getLoc(), op.getCallee(), llvm::None,
                                       buffers);
-
-        // Erase or replace previous operations
-        if (isFuncRet)
-            op.erase();
-        else
-            rewriter.replaceOp(op, newResults);
-
         return success();
     }
 };
@@ -152,7 +221,7 @@ LogicalResult LowerFunc::matchAndRewrite(func::FuncOp func,
             for (auto result : newOp->getResults())
                 result.setType(cvtTensorToMemref(result.getType()));
 
-            // Deallocate arguments which is lastly used by this operation
+            // Deallocate arguments which are lastly used by this operation
             if (op.getName().getStringRef() ==
                 func::ReturnOp::getOperationName())
                 continue;
